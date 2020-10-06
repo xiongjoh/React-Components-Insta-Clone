70df5704f693897866e55fa67314361161287d0a
unknown <xiongjoh@gmail.com>
2020-10-06 16:01:05 -0500

    add function in LikeSection.js to only allow liking once

--- src/components/Posts/LikeSection.js
+++ src/components/Posts/LikeSection.js
@@ -2,14 +2,27 @@
 // Use a piece of data coming in through props to display the correct number of likes.
 // You will also add an onClick handler that utilizes `likePost` to increase the count of likes.
 // (As a stretch goal, you might want to prevent your user from "liking" the same post more than once.)
-import React/*, { useState }*/ from 'react';
+import React, {useState} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faComment, faHeart } from '@fortawesome/free-regular-svg-icons';
 
 const LikeSection = props => {
   // 🔥 Make sure the parent of LikeSection is passing the right props!
   const { likePost, numberOfLikes } = props;
-  // const [isPostLiked, setIsPostLiked] = useState(false)
+  let [postLiked, setPostLiked] = useState(false)
+
+  function runFunctions() {
+    
+    if (postLiked){
+      return null
+    }
+    else {
+      likePost()
+      setPostLiked(postLiked = true)
+    }
+    
+  }
+
   return (
     <div>
       <div
@@ -17,7 +30,9 @@ const LikeSection = props => {
         key='likes-icons-container'
       >
         <div className='like-section-wrapper'>
-          <FontAwesomeIcon icon={faHeart} onClick={/*isPostLiked? (console.log("Post is Liked")): (likePost)*/ likePost}/>
+
+            <FontAwesomeIcon icon={faHeart} onClick={runFunctions}/>
+
         </div>
         <div className='like-section-wrapper'>
           <FontAwesomeIcon icon={faComment} />
